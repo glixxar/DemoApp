c54e93cac6a72706bc51938a7cd9743f2ddd9ff1
glixxar <alvinyii123@gmail.com>
2024-12-25 11:17:42 +1000

    added endpoints

--- DemoApp.Server/Program.cs
+++ DemoApp.Server/Program.cs
@@ -9,22 +9,23 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddAuthorization();
+        builder.Services.AddIdentityApiEndpoints<User>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        builder.Services.AddAuthorization();
-        builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+        //builder.Services.AddAuthentication()
+        //    .AddCookie(IdentityConstants.ApplicationScheme);
 
-        builder.Services.AddIdentityCore<User>()
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddApiEndpoints();
-
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        //builder.Services.AddIdentityCore<User>()
+        //    .AddEntityFrameworkStores<ApplicationDbContext>()
+        //    .AddApiEndpoints();
 
         var app = builder.Build();
 
